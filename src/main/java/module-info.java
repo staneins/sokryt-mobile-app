5c2105ff8eaff114d1f11b710d1ac1939e5c59a4
Philip Kaminsky <kaminsky8836@gmail.com>
2024-07-11 09:14:13 +0300

    remove gluon structure

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module kaminsky.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    exports service;
+    opens service to javafx.fxml;
+    exports controller;
+    opens controller to javafx.fxml;
+}
